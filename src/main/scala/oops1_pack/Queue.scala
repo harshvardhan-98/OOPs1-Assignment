791c536a91b13e2dc915a30ff53e025822ee7df1
Harsh <harsh.vardhan@knoldus.com>
2021-08-06 17:05:54 +0530

    Initial commit

---
+++ src/main/scala/oops1_pack/Queue.scala
@@ -0,0 +1,18 @@
+package oops1_pack
+import scala.collection.mutable.ListBuffer
+
+
+trait Queue {
+
+  def enqueue(elementToBePushed : Int)
+
+  def dequeue(queue: ListBuffer[Int]) : Int ={
+
+    val removedElement = queue(0)
+    queue.remove(0)
+    removedElement
+  }
+
+}
+
+
---
+++ src/main/scala/oops1_pack/SquareQueue.scala
@@ -0,0 +1,20 @@
+package oops1_pack
+import scala.collection.mutable.ListBuffer
+
+class SquareQueue(queue: ListBuffer[Int]) extends Queue {
+
+  override def enqueue(elementToBePushed: Int): Unit = {
+
+    queue += (elementToBePushed * elementToBePushed)
+  }
+
+}
+
+class DoubleQueue(queue : ListBuffer[Int]) extends Queue {
+
+  override def enqueue(elementToBePushed: Int): Unit = {
+
+    queue += (elementToBePushed * 2)
+  }
+
+}
